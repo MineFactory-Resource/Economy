c8ea8c19f5d2c59670fb84e98c7d878726906471
Seokhun-Yang <ddayang1234@gmail.com>
2022-09-01 17:39:22 +0900

    moneydata.yml 관리 클래스 추가

--- src/main/java/net/teamuni/economy/Uconomy.java
+++ src/main/java/net/teamuni/economy/Uconomy.java
@@ -6,8 +6,7 @@ public final class Uconomy extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        saveDefaultConfig();
     }
 
     @Override
