6a294f12ccf094f51ec5689f57e91a3526f63af7
Seokhun-Yang <ddayang1234@gmail.com>
2022-09-19 20:10:29 +0900

    불필요한 import 제거

--- src/main/java/net/teamuni/economy/hooks/HookIntoVault.java
+++ src/main/java/net/teamuni/economy/hooks/HookIntoVault.java
@@ -2,17 +2,15 @@ package net.teamuni.economy.hooks;
 
 import net.milkbowl.vault.economy.Economy;
 import net.teamuni.economy.Uconomy;
-import net.teamuni.economy.data.EconomyManager;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.ServicePriority;
 
 public class HookIntoVault {
     private final Uconomy plugin = Uconomy.getInstance();
-    private final EconomyManager manager = Uconomy.getEconomyManager();
     private Economy provider;
 
     public void hook() {
-        provider = manager;
+        provider = Uconomy.getEconomyManager();
         Bukkit.getServicesManager().register(Economy.class, provider, this.plugin, ServicePriority.High);
         Bukkit.getLogger().info("[Uconomy] Vault is hooked into " + plugin.getName() + " sucessfully!");
     }
