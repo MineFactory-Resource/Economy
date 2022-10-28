1053382a05897057de20c45bf6c57ae71507dcdb
Seokhun-Yang <ddayang1234@gmail.com>
2022-10-28 20:56:38 +0900

    PlayerData 클래스에 afterDeposit, afterWithdraw 메소드 추가

--- src/main/java/net/teamuni/economy/data/PlayerData.java
+++ src/main/java/net/teamuni/economy/data/PlayerData.java
@@ -1,4 +1,17 @@
 package net.teamuni.economy.data;
 
-public record PlayerData(String uuid, long money) {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class PlayerData {
+    private final String uuid;
+    private long money;
+    public void afterDeposit(long depositedMoney) {
+        this.money = depositedMoney;
+    }
+    public void afterWithdraw(long withDrewMoney) {
+        this.money = withDrewMoney;
+    }
 }
