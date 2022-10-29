94b0f46fa0861267b97729125c2e043f706bf2b4
Seokhun-Yang <ddayang1234@gmail.com>
2022-10-29 20:47:07 +0900

    캐시가 존재하지 않는 경우에만 로드 롤백

--- src/main/java/net/teamuni/economy/event/JoinEvent.java
+++ src/main/java/net/teamuni/economy/event/JoinEvent.java
@@ -1,7 +1,6 @@
 package net.teamuni.economy.event;
 
 import net.teamuni.economy.Uconomy;
-import net.teamuni.economy.data.PlayerDataManager;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -19,10 +18,7 @@ public class JoinEvent implements Listener {
     public void onJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
         Bukkit.getScheduler().runTaskAsynchronously(main, () -> {
-            PlayerDataManager playerDataManager = main.getPlayerDataManager();
-            if (playerDataManager.getCacheIfPresent(player.getUniqueId()) == null) {
-                playerDataManager.getCache(player.getUniqueId());
-            }
+            main.getPlayerDataManager().getCache(player.getUniqueId());
             if (!main.getEconomyManager().hasAccount(player)) {
                 main.getEconomyManager().createPlayerAccount(player);
                 Bukkit.getLogger().info("[Uconomy] " + player.getName() + "님의 돈 정보를 생성하였습니다.");
