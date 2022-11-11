bb3818c87661e2ab276c29817b373143b3620ae2
Seokhun-Yang <ddayang1234@gmail.com>
2022-11-11 21:20:32 +0900

    PlayerData 클래스 String에서 UUID로 변경

--- src/main/java/net/teamuni/economy/data/PlayerData.java
+++ src/main/java/net/teamuni/economy/data/PlayerData.java
@@ -3,10 +3,12 @@ package net.teamuni.economy.data;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.util.UUID;
+
 @Data
 @AllArgsConstructor
 public class PlayerData {
-    private final String uuid;
+    private final UUID uuid;
     private long money;
     public void afterDeposit(long depositedMoney) {
         this.money = depositedMoney;
